aa0d51dfdc635b646f8997d0dbe9b55e95c6a821
ROCPiXel <60724881+ROCmirs@users.noreply.github.com>
2025-09-04 19:09:41 +0800

    Merge commit 'e93aa7b1cff4216e7e76bc5d6bc98006d1e8b3d2'

---
+++ Utils/ObjectHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace Synx.Common.Utils
+{
+    public static class ObjectHelper
+    {
+        /// <summary>
+        /// 交换两个对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static void Change<T>(ref T a, ref T b)
+        {
+            (a, b) = (b, a);
+        }
+    }
+}
